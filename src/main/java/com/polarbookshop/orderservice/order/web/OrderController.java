b1296049c28557a8b462bc15342b80ff654afbac
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-05-20 11:40:56 +0000

    first commit

---
+++ src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
@@ -0,0 +1,35 @@
+package com.polarbookshop.orderservice.order.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.polarbookshop.orderservice.order.domain.Order;
+import com.polarbookshop.orderservice.order.domain.OrderService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping()
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(@RequestBody OrderRequest orderRequest) {
+        return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
+    }
+
+}
---
+++ src/test/java/com/polarbookshop/orderservice/web/OrderControllerWebFluxTests.java
@@ -0,0 +1,50 @@
+package com.polarbookshop.orderservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.polarbookshop.orderservice.order.domain.Order;
+import com.polarbookshop.orderservice.order.domain.OrderService;
+import com.polarbookshop.orderservice.order.domain.OrderStatus;
+import com.polarbookshop.orderservice.order.web.OrderController;
+import com.polarbookshop.orderservice.order.web.OrderRequest;
+
+import reactor.core.publisher.Mono;
+
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebFluxTest(OrderController.class)
+public class OrderControllerWebFluxTests {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    void whenBookNotAvailableThenRejectOrder() {
+        var orderRequest = new OrderRequest("1234567890", 3);
+        var expectedOrder = OrderService.buildRejectedOrder(orderRequest.isbn(), orderRequest.quantity());
+        given(orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity()))
+                .willReturn(Mono.just(expectedOrder));
+
+        webClient.post()
+                .uri("/orders")
+                .bodyValue(orderRequest)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Order.class).value(actualOrder -> {
+                    assertThat(actualOrder).isNotNull();
+                    assertThat(actualOrder.bookIsbn()).isEqualTo(expectedOrder.bookIsbn());
+                    assertThat(actualOrder.status()).isEqualTo(OrderStatus.REJECTED);
+                });
+
+    }
+
+}
